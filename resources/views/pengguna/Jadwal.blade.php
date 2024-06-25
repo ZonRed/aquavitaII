91a5d167fb1dc083cf2eb628ea50c350809e0566
ZonRed <myansar70@gmail.com>
2024-06-25 23:49:27 +0700

    realtime pengguna jadwal

--- resources/views/pengguna/Jadwal.blade.php
+++ resources/views/pengguna/Jadwal.blade.php
@@ -114,6 +114,7 @@
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
 
+   <!-- script ajax table,pagination,dan search -->
     <script>
         $(document).ready(function() {
             let currentPage = 1;
